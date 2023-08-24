60f2c401f1a46269f04dde9003924d3b082d0541
elrizoos <77412223+elrizoos@users.noreply.github.com>
2023-08-24 19:36:33 +0200

    Ahora se puede añadir datos aleatorios en la base de datos y añadida la paginacion en las tablas

--- database/migrations/2023_08_22_174305_create_horarios_table.php
+++ database/migrations/2023_08_22_174305_create_horarios_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('horarios', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('codigoClase');
+            $table->unsignedBigInteger('codigoGrupo');
+            $table->unsignedBigInteger('codigoEmpleado');
+            $table->string('diaSemana');
+            $table->datetime('horaInicio');
+            $table->datetime('horaFin');
+            $table->date('fechaExclusiva');
+            $table->foreign('codigoClase')->references('id')->on('clases');
+            $table->foreign('codigoGrupo')->references('id')->on('grupos');
+            $table->foreign('codigoEmpleado')->references('id')->on('empleados');
             $table->timestamps();
         });
     }
